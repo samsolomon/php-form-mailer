a3a9aeb9488c08669e3b466f5c1ac6cda71a9458
Sam Solomon <samuelrobertsolomon@gmail.com>
2013-06-10 14:23:49 -0500

    second commit

---
+++ form-submission.php
@@ -0,0 +1,94 @@
+<?php
+
+// SUGGESTED FORM RESOURCES
+//--------------------------
+// - http://rosstanner.co.uk/2012/11/build-simple-contact-form-html-php-bootstrap/ (Primiary source.)
+// - http://alittlecode.com/jquery-form-validation-with-styles-from-twitter-bootstrap/
+// - http://davidwalsh.name/password-protect-directory-using-htaccess
+// - http://www.limelightonline.co.nz/blog/create-dynamic-pdf-with-php-tutorial/
+
+
+
+// check for form submission - if there is no submission redirect back to the form
+if (!isset($_POST["save"]) || $_POST["save"] != "contact") {
+    header("Location: form.php"); exit;
+}
+	
+// get the posted data, and save as variables
+$today_date = date('m/d/Y');
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+
+$option_a = $_POST["option_a"];
+$option_b = $_POST["option_b"];
+$option_c = $_POST["option_c"];
+
+
+//recipients
+$ron = $_POST["ron"];
+$brian = $_POST["brian"];
+$champ = $_POST["champ"];
+$brick = $_POST["brick"];
+
+
+
+// check that a job name was entered  
+if (empty($name))  {
+    $error = "Please enter a your name.";
+} 
+// check for a valid email address  
+elseif (!preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email_address))  
+	$error = "You must enter a valid email address.";  
+
+// check if an error was found - if there was, send the user back to the form  
+if (isset($error)) {  
+    header("Location: form.php?e=".urlencode($error)); exit;  
+}  
+
+
+
+
+		
+// write the email content
+$email_content .= "Send Date: $today_date\n";
+
+$email_content = "Name: $name\n";
+$email_content .= "email: $email\n";
+
+if ($option_a==!null) {
+$email_content .= "Option A: $option_a\n";
+}
+if ($option_b==!null) {
+$email_content .= "Option B: $option_b\n";
+}
+if ($option_c==!null) {
+$email_content .= "Option C: $option_c\n";
+}
+if ($message==!null) {
+$email_content .= "Message: \n$message\n";
+}
+
+
+
+
+// where to send the email
+// mail ("email address", subject, content);
+if ($ron==!null) {
+	mail ("ron@example.com", $name, $email_content);
+}
+if ($elena==!null) {
+	mail ("brian@example.com", $name, $email_content);
+}
+if ($jamie==!null) {
+	mail ("champ@example.com", $name, $email_content);
+}
+if ($jamie==!null) {
+	mail ("brick@example.com", $name, $email_content);
+}
+
+	
+// send the user back to the form
+header("Location: form.php?s=".urlencode("Job Order Submitted.")); exit;
+
+?>
---
+++ form.php
@@ -0,0 +1,137 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>PHP Form Mailer/title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- CUSTOM STYLES -->
+<style type="text/css">
+	.controls.left{
+		margin-left: 0;
+	}
+</style>
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+	<div class="container">
+		<div class="span12">
+	
+        <div class="page-header">
+            <h1>PHP Form Mailer</h1>
+        </div>
+<?php
+
+		// check for a successful form post
+		if (isset($_GET['s'])) echo "<div class=\"alert alert-success\">".$_GET['s']."</div>";
+
+		// check for a form error
+		elseif (isset($_GET['e'])) echo "<div class=\"alert alert-error\">".$_GET['e']."</div>";
+
+?>
+
+        <form method="POST" action="form-submission.php" class="form-horizontal">
+
+
+<!-- ==============================================
+		TEXT FIELD
+============================================== -->
+            <div class="control-group">
+                <label class="control-label" for="name">Name</label>
+                <div class="controls">
+                    <input type="text" name="job_name" id="name" placeholder="Your Name">
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label" for="email">email</label>
+                <div class="controls">
+                    <input type="email" name="email" id="email" placeholder="Your Email">
+                </div>
+            </div>
+
+
+<!-- ==============================================
+		CHECKBOX MESSAGE
+============================================== -->
+			<div class="page-header">
+	            <h2>Checkboxes</h2>
+	        </div>
+
+		        <label class="checkbox">
+					<input type="checkbox" name="option_a" value="Yes">
+				  	Option A
+				</label>
+
+		        <label class="checkbox">
+					<input type="checkbox" name="option_b" value="Yes">
+				  	Option B
+				</label>
+
+				<label class="checkbox">
+					<input type="checkbox" name="option_c" value="Yes">
+				  	Option C
+				</label>
+			</div>
+
+<!-- ==============================================
+		TEXT AREA MESSAGE
+============================================== -->
+			<div class="page-header">
+	            <h2>Text Area</h2>
+	        </div>			
+
+	        <div class="control-group"> 
+                <label class="control-label" for="message">Message</label>  
+                <div class="controls left">
+                    <textarea name="text_area" id="message" rows="8" class="message" placeholder="Your Message"></textarea>
+                </div>
+            </div>
+
+<!-- ==============================================
+		CHOOSE RECIPIENTS
+============================================== -->            
+	        <div class="page-header">
+	            <h2>Order Recipients</h2>
+	        </div>
+
+				<label class="checkbox">
+					<input type="checkbox" name="ron" value="Yes">
+				  	Ron Burgandy 
+				</label>
+
+				<label class="checkbox">
+					<input type="checkbox" name="brian" value="Yes">
+				  	Brian Fantana
+				</label>
+
+				<label class="checkbox">
+					<input type="checkbox" name="champ" value="Yes">
+				  	Champ Kind 
+				</label>
+
+				<label class="checkbox">
+					<input type="checkbox" name="brick" value="Yes">
+				  	Brick Tamland
+				</label>
+
+<!-- ==============================================
+		SUBMIT BUTTON
+============================================== -->
+            <div class="form-actions">
+                <input type="hidden" name="save" value="contact">
+                <button type="submit" class="btn btn-large btn-primary">Submit</button>
+            </div>
+        </form>
+        
+    	</div> <!-- end span -->
+    </div> <!-- end container -->
+
+
+</body>
+</html>
